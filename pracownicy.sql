89784e013570bd324c3de1d6627d37b63c98bd0a
the-realcar <re4lcar@gmail.com>
2025-11-24 12:44:16 +0100

    Update pracownicy.sql

--- pracownicy.sql
+++ pracownicy.sql
@@ -1,2 +1,3 @@
-INSERT INTO pracownicy (imie, nazwisko, nazwa, id_stanowiska, id_zajezdni)
-VALUES ('Dawid','Volve','the_realcar','14','5');
+INSERT INTO pracownicy (imie, nazwisko, login, password, id_stanowiska, id_zajezdni, rola_sys, discord_id, konto_aktywne)
+VALUES ('Dawid','Volve','the_realcar', 'Zajezdnia12345!!', '14','5', '1', '', TRUE),
+('Jakub Hubert', 'Trniecki', 'Kustul', 'gfvbieoh2tg5!ahg', '13', '5', '1', '', TRUE);
